7449c42ea5f49c738c614ef1f0c1c3ab1dc18498
gogster27@gmail.com <Fhntv267094>
2020-02-05 16:06:10 +0300

    - add ppi shablon creation

---
+++ src/main/java/pages/PpiSamplePage.java
@@ -0,0 +1,128 @@
+package pages;
+
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.By;
+import static java.lang.String.format;
+import static org.openqa.selenium.By.xpath;
+
+@DefaultUrl("https://a2-test.gpnsmonitor.ru/")
+public class PpiSamplePage extends PageObject {
+    AdminLoginPage page;
+
+    private By menu_ppi_sample = By.xpath("//body//a[4]");
+    private By create_button = By.xpath("//div[contains(text(),'Создать')]");
+    private By ppi_name_input = By.xpath("//input[@type='text']");
+    private By provider_name_input = By.xpath("//input[@label='Введите поставщика']");
+    private By product_name_input = By.xpath("//input[@label='Выберите изделия']");
+
+    private By group_operation_input = By.xpath("//input[@label='Группа технологических операций']");
+    private By group_operation = By.xpath("//div[contains(text(),'Производство')]");
+    private By operation_input = By.xpath("//input[@label='Технологическая операция']");
+    private By operation = By.xpath("//div[contains(text(),'Контроль испытательного оборудования')]");
+    private By add_operation_button = By.xpath("//div[contains(text(),'Добавить операцию')]");
+
+    private By checking_characteristics_input = By.xpath("//input[@label='Проверяемые характеристики']");
+    private By criteria_acceptance_input = By.xpath("//input[@label='Критерии приемки']");
+    private By registration_method_input = By.xpath("//input[@label='Метод регистрации результатов']");
+    private By inspector_action_input = By.xpath("//input[@label='Действия инспектора при несоответствии']");
+    private By link_document_text = By.xpath("//textarea[@name='ref-docs']");
+
+    private String provider_name = "//span[contains(text(),'%s')]";
+    private String product_name = "//span[contains(text(),'%s')]";
+    private String created_operation_group = "//div[@class='operations-list expansion-panel v-item-group theme--light v-expansion-panels v-expansion-panels--focusable']//div[%s]//button[1]";
+    private String created_operation = "//div[@class='v-expansion-panel sub-operation'][%s]";
+    private String choose_characteristic = "//span[contains(text(),'%s')]";
+
+    public void login(String email, String pass){page.authorization(email, pass);}
+
+    public void chooseMenuPpiSample(){
+        find(menu_ppi_sample).waitUntilClickable().click();
+        //waitABit(500);
+    }
+
+    public void createButtonClick(){
+        find(create_button).waitUntilClickable().click();
+    }
+
+    public void typePpiSampleName(String name){
+        find(ppi_name_input).sendKeys(name);
+    }
+
+    public void chooseProvider(String name){
+        find(provider_name_input).sendKeys(name);
+        element(xpath(format(provider_name,name))).click();
+    }
+
+    public void chooseProduct(String prod_name){
+        find(product_name_input).sendKeys(prod_name);
+        element(xpath(format(product_name,prod_name))).click();
+    }
+
+    public void chooseGroupOperation(){
+        find(group_operation_input).click();
+        find(group_operation).click();
+    }
+    public void chooseOperation(){
+        find(operation_input).click();
+        find(operation).click();
+    }
+    public void addOperationButtonClick(){
+        find(add_operation_button).waitUntilClickable().click();
+    }
+
+    public void chooseCreatedOperationGroup(String group_pos){
+        element(xpath(format(created_operation_group,group_pos))).click();
+    }
+
+    public void chooseCreatedOperation(String oper_pos){
+        element(xpath(format(created_operation,oper_pos))).click();
+    }
+
+    public void chooseCharacteristics(String check_char, String criterion_acceptance, String reg_method, String insp_action){
+        find(checking_characteristics_input).sendKeys(check_char);
+        element(xpath(format(choose_characteristic,check_char))).click();
+
+        find(criteria_acceptance_input).sendKeys(criterion_acceptance);
+        element(xpath(format(choose_characteristic,criterion_acceptance)));
+
+        find(registration_method_input).sendKeys(reg_method);
+        element(xpath(format(choose_characteristic,reg_method)));
+
+        find(inspector_action_input).sendKeys(insp_action);
+        element(xpath(format(choose_characteristic,insp_action)));
+    }
+
+    public void chooseFrequencyMethod(){
+        findAll(By.xpath("//input[@label='Метод проверки']")).get(0).click();
+        find(By.xpath("//div[contains(text(),'R (проведение проверки документации)')]")).click();
+
+        findAll(By.xpath("//input[@label='Метод проверки']")).get(1).click();
+        find(By.xpath("//div[contains(text(),'R (проведение проверки документации)')]")).click();
+
+        findAll(By.xpath("//input[@label='Метод проверки']")).get(2).click();
+        find(By.xpath("//div[contains(text(),'R (проведение проверки документации)')]")).click();
+
+        findAll(By.xpath("//input[@label='Частота проверки']")).get(0).click();
+        find(By.xpath("//div[contains(text(),'B (выборочно)')]")).click();
+
+        findAll(By.xpath("//input[@label='Частота проверки']")).get(1).click();
+        find(By.xpath("//div[contains(text(),'B (выборочно)')]")).click();
+
+        findAll(By.xpath("//input[@label='Частота проверки']")).get(2).click();
+        find(By.xpath("//div[contains(text(),'B (выборочно)')]")).click();
+
+
+        findAll(By.xpath("//button[contains(text(),'Добавить')]")).get(0).click();
+        findAll(By.xpath("//button[contains(text(),'Добавить')]")).get(1).click();
+        findAll(By.xpath("//button[contains(text(),'Добавить')]")).get(2).click();
+        waitABit(1000);
+    }
+
+    public void typeLinkDocument(String text){
+        find(link_document_text).sendKeys(text);
+        find(By.xpath("//button[contains(text(),'Сохранить документ')]"));
+    }
+
+
+}
---
+++ src/main/java/steps/PpiSampleSteps.java
@@ -0,0 +1,39 @@
+package steps;
+
+import net.thucydides.core.annotations.Step;
+import pages.PpiSamplePage;
+
+public class PpiSampleSteps {
+    PpiSamplePage page;
+
+    @Step
+    public void open_page(){page.open();}
+    @Step
+    public void choose_menu_ppi(){page.chooseMenuPpiSample();}
+    @Step
+    public void create_button_click(){page.createButtonClick();}
+    @Step
+    public void type_sample_name(String name){page.typePpiSampleName(name);}
+    @Step
+    public void choose_provider(String prov_name){page.chooseProvider(prov_name);}
+    @Step
+    public void choose_product(String prod_name){page.chooseProduct(prod_name);}
+    @Step
+    public void choose_group_operation(){page.chooseGroupOperation();}
+    @Step
+    public void choose_operation(){page.chooseOperation();}
+    @Step
+    public void add_operation_button_click(){page.addOperationButtonClick();}
+    @Step
+    public void choose_created_operation_group(String position){page.chooseCreatedOperationGroup(position);}
+    @Step
+    public void choose_created_operation(String position){page.chooseCreatedOperation(position);}
+    @Step
+    public void choose_characteristics(String check_char, String criterion_acceptance, String reg_method, String insp_action ){
+        page.chooseCharacteristics(check_char, criterion_acceptance, reg_method, insp_action);
+    }
+    @Step
+    public void choose_frequency_method(){page.chooseFrequencyMethod();}
+    @Step
+    public void type_link_document(String text){page.typeLinkDocument(text);}
+}
